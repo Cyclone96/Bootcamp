f460ff2c73ec9d9e9edcf855dcbe1204b9e7e27d
Cyclone96 <86723600+Cyclone96@users.noreply.github.com>
2021-08-02 14:07:43 +0300

    Add files via upload

---
+++ Talklikeaprincipal.py
@@ -0,0 +1,41 @@
+print("--------THE SCHOOL PRINCIPAL---------")
+
+def nounWord():
+    print("enter nouns")
+    nounWords=[]
+
+    for n in range(5):
+        input1=input("{}".format(n+1))
+        nounWords.append(input1: )
+
+    return nounWords
+
+
+def verbword():
+    print("enter verbs")
+    verbwords = []
+
+    for n in range(5):
+        input1 = input("[{}".format(n + 1))
+        verbwords.append(input1)
+
+    return verbwords
+
+def adjword():
+    print("enter adjectives")
+    adjwords = []
+
+    for n in range(5):
+        input1 = input("{}".format(n + 1))
+        adjwords.append(input1)
+
+    return adjwords
+
+
+nounwords1 = nounWord()
+verbwords1 = verbword()
+adjwords1 = adjword()
+
+
+
+principlestory="Good Morning School."+nounwords1[0]+".It has been a long week but I am "+verbwords1[2]+"to see you're all "+adjwords1[3]+"I appreciate"+"your continued commiment"+adjwords1[1]+"It is my honour to officially"+verbwords1[2]+"that we will close school on the 5th of August, 2021. I wish you a"+adjwords1[4]+nounwords1[3]
